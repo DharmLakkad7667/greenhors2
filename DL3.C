c18602322bed5bf1422ef62c996cbb9276cbb612
DharmLakkad7667 <132447689+DharmLakkad7667@users.noreply.github.com>
2023-05-04 15:36:49 +0530

    Add files via upload

---
+++ DL3.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+clrscr();
+ int a,b;
+ printf("enter value a=");
+ scanf("%d",&a);
+ printf("enter value n=");
+ scanf("%d",&b);
+ printf("addition of a+b=%d",a+b);
+gatch();
+}
